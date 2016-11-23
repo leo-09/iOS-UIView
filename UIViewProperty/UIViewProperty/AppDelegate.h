fe2ad42a4acf4e7866377ded946a401c3257d19f
liyy <1290374862@qq.com>
2016-11-23 23:01:22 +0800

    iOS UIView的frame，bounds，center，position，anchorPoint的实践

---
+++ UIViewProperty/UIViewProperty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewProperty
+//
+//  Created by liyangyang on 2016/11/23.
+//  Copyright © 2016年 liyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewProperty/UIViewProperty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewProperty
+//
+//  Created by liyangyang on 2016/11/23.
+//  Copyright © 2016年 liyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
